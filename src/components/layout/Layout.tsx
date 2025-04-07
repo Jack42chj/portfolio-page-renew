9dc109f2a7c0000cf9c92538397c174b59e31aa8
CHOI HOJIN <hojinch99@gmail.com>
2025-04-07 22:47:51 +0900

    feat: 공용 layout 컴포넌트 제작

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import * as S from "./layout.styled";
+
+interface LayoutBase {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const Layout = ({ className, children }: LayoutBase) => {
+  return <S.Layout className={className}>{children}</S.Layout>;
+};
+
+export default Layout;
