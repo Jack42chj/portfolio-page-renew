e503c86cb88833d1682fa4b477db46f00cfc2e41
CHOI HOJIN <hojinch99@gmail.com>
2025-04-09 23:29:51 +0900

    style: 공용 색상 및 폰트 추가

--- src/styles/themes/colors.ts
+++ src/styles/themes/colors.ts
@@ -3,6 +3,7 @@ export const colors = {
   black: "#000000",
   blue_primary: "#ACCCEE",
   blue_secondary: "#8FB3D6",
+  blue_thrid: "##6C9BCF",
 } as const;
 
 export type ColorType = typeof colors;
